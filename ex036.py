90ed6d18dd033fc7931d05f6f58ccf7fcd81273a
MidoriyaDEV <victornascimentobr48@gmail.com>
2024-09-12 17:45:50 -0300

    Upgrade

--- ex036.py
+++ ex036.py
@@ -5,9 +5,10 @@ Prestação = Casa / (Anos * 12)
 Minimo = Salario * 30 / 100
 print('Para pagar uma casa de {:.2f} em {} anos'.format(Casa, Anos), end ='')
 print('a prestão será de R${:.2f}'.format(Prestação))
-
-print('COMPARANDO tem que pagar {:.2f} e o mínimo é de  {:.2f}'.format(Prestação, Minimo))
-
+if Prestação <= Minimo:
+    print('Emprestimo pode ser CONCEDIDO!')
+else:
+    print('Emprestimo NEGADO!')
 
 #Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa. 
 #Pergunte o valor da casa, o salário do comprador e em quantos anos ele vai pagar. 
