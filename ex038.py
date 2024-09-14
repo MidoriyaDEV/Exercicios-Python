ad1a432406466436f684c9562fd3f5c72c527d8c
MidoriyaDEV <victornascimentobr48@gmail.com>
2024-09-13 23:06:55 -0300

    New

---
+++ ex038.py
@@ -0,0 +1,27 @@
+print('Escolha dois números')
+n1 = int(input('Prmeiro número: ')) 
+n2 = int(input('Segundo Número: '))
+if n1 > n2:
+    print('O primeiro valor é maior')
+elif n2 > n1:
+    print('O segundo valor é maior')
+elif n1 == n2:
+    print('Os dois valores são iguais!')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Escreva um programa em Python que leia um número inteiro qualquer e peça para o usuário escolher qual será a base de conversão: 1 para binário, 2 para octal e 3 para hexadecimal.
